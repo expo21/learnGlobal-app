f1a31b39e49182743deaa3df55785c2dfea0c00b
expo21 <rajat.expinator21@gmail.com>
2021-05-07 14:06:35 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.get("/home", (req, res) => {
+  console.log("hdfgjsgf");
+  res.send({ message: "sdhjdgdfjshd" });
+});
+app.listen(3636, () => {
+  console.log(`server running on port 3636`);
+});
