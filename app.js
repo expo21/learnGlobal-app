c2806cc372089296833f3e0a5ae7492aca8a074a
expo21 <rajat.expinator21@gmail.com>
2021-05-07 14:41:52 +0530

    updated app.js

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(3636, () => {
+app.listen(process.env.PORT || 3636, () => {
   console.log(`server running on port 3636`);
 });
