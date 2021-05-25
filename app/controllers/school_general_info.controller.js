5e5b8a2986c174e81e9b28e94afb6af1edf6510c
expo21 <rajat.expinator21@gmail.com>
2021-05-25 12:08:12 +0530

    top universities

--- app/controllers/school_general_info.controller.js
+++ app/controllers/school_general_info.controller.js
@@ -1,4 +1,7 @@
 const SchoolGeneralInfo = require("../models/school_general_info.model");
+const SchoolAbout = require("../models/school_about.model");
+const Json2csvParser = require("json2csv").Parser;
+const fs = require("fs");
 
 // get school info by id
 const get_school_info_by_id = async (id) => {
@@ -104,7 +107,15 @@ const discover_all_schools = async () => {
         },
       },
     ]);
-    return schools;
+    if (schools.length > 0) {
+      const json2csvParser = new Json2csvParser({ header: true });
+      const csvData = json2csvParser.parse(schools);
+      fs.writeFile("mongodb_fs.csv", csvData, function (error) {
+        if (error) throw error;
+        console.log("Write to bezkoder_mongodb_fs.csv successfully!");
+      });
+      return schools;
+    } else false;
   } catch (error) {
     throw error;
   }
@@ -113,42 +124,48 @@ const discover_all_schools = async () => {
 //get random schools
 const get_random_schools = async () => {
   try {
-    let random_schools = await SchoolGeneralInfo.aggregate([
+    let random_schools = await SchoolAbout.aggregate([
+      { $match: { total_student: { $gte: 10000 } } },
+
       { $sample: { size: 10 } },
       {
         $lookup: {
-          from: "SchoolAbout",
-          localField: "id",
-          foreignField: "school_genral_id",
-          as: "school_about_info",
+          from: "SchoolGeneralInfo",
+          localField: "school_genral_id",
+          foreignField: "id",
+          as: "school_general_info",
         },
       },
       {
         $lookup: {
           from: "SchoolLogo",
-          localField: "school_about_info.id",
+          localField: "school_general_info.id",
           foreignField: "school_about_id",
           as: "school_logo",
         },
       },
-      { $unwind: "$school_about_info" },
+      { $unwind: "$school_general_info" },
       { $unwind: "$school_logo" },
       {
         $project: {
           id: 1,
-          school_name: 1,
-          country: 1,
-          "school_about_info.total_student": 1,
+          "school_general_info.school_name": 1,
+          "school_general_info.id": 1,
+          "school_general_info.country": 1,
+          total_student: 1,
           "school_logo.logo": 1,
         },
       },
     ]);
+    console.log(random_schools);
     if (random_schools.length > 0) {
       return random_schools;
     } else {
       return false;
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
